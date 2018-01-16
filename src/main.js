d46f8cf21ca7901ae0ea8d3a4abb865ef3297580
ShahPar <shhrrparvez@gmail.com>
2018-01-16 00:41:07 -0500

    fixed theme rendering

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import App from './App'
 import router from './router'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css' // This line here
 
 Vue.use(VueMaterial)
 
