cde9e0960553308c932e19faf05586a6963fc4f1
ShahPar <shhrrparvez@gmail.com>
2018-06-14 15:54:19 -0400

    updated bio

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,7 +6,7 @@
           <div id="bio">
             <strong>Designer</strong>, <strong>Developer</strong>, and <strong>Tech Enthusiast</strong> <br />
             Studied <strong>Computer Science</strong> at <strong>Stony Brook University</strong> <br />
-            Proficient in <strong>Java</strong>, <strong>Python</strong>, <strong>C</strong>, and Klingon <br />
+            Proficient in <strong>Java</strong>, <strong>JavaScript</strong>, <strong>Python</strong>, and Klingon <br />
             Experience with <strong>Angular</strong> and <strong>Spring Boot</strong> <br/><br/>
             - Also - <br/><br/>
             Former Londoner and current New Yorker<br />
