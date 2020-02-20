02cd1bb65a96806123b936cf73ebeab52aacc3f6
ssparvez <shhrrparvez@gmail.com>
2020-02-20 14:00:46 -0500

    updated about

--- src/components/About.vue
+++ src/components/About.vue
@@ -7,7 +7,7 @@
             <strong>Designer</strong>, <strong>Developer</strong>, and <strong>Tech Enthusiast</strong> <br />
             Studied <strong>Computer Science</strong> at <strong>Stony Brook University</strong> <br />
             Proficient in <strong>Java</strong>, <strong>JavaScript</strong>, <strong>Python</strong>, and Klingon <br />
-            Experience with <strong>Angular</strong> and <strong>Spring Boot</strong> <br/><br/>
+            Experience with <strong>React</strong>, <strong>Angular</strong> <strong>Node.js</strong> and <strong>Spring Boot</strong> <br/><br/>
             - Also - <br/><br/>
             Former Londoner and current New Yorker<br />
             Always supporting Liverpool FC and jamming to Tame Impala<br /><br />
